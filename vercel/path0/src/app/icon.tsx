88fd07f6f5309e7c7791c64ab00d353ed72b6c7d
Poultry Mitra <ipoultrymitra@gmail.com>
2025-10-13 11:57:11 +0000

    ab ye error hai 17:24:52.857 Running build in Washington, D.C., USA (Ea

---
+++ vercel/path0/src/app/icon.tsx
@@ -0,0 +1,23 @@
+
+import type { SVGProps } from 'react';
+
+export function AppIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+        >
+          <path d="M16 7h.01" />
+          <path d="M3.4 18H12a8 8 0 0 0 8-8V7a4 4 0 0 0-7.28-2.3L2 20" />
+          <path d="m20 7 2 .5-2 .5" />
+          <path d="M10 18v3" />
+          <path d="M14 18v3" />
+        </svg>
+    )
+}
