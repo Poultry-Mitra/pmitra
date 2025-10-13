4ab00fa7a12f4269ece18bd4dcea956cb2d1876a
Poultry Mitra <ipoultrymitra@gmail.com>
2025-10-13 10:04:49 +0000

    maine custome domain add kar diya hai poultrymitra.com beckend mai mai c

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-2759667395-3e284",
   "appId": "1:219591700993:web:526879e1a92724745f0600",
   "apiKey": "AIzaSyDiBc43ILht21w2qlSWRnEYWdAWVAoY9oo",
-  "authDomain": "studio-2759667395-3e284.firebaseapp.com",
+  "authDomain": "poultrymitra.com",
   "measurementId": "",
   "messagingSenderId": "219591700993"
 };
