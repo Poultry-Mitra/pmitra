b262c2ece103b09a25603c5244a6bf9798c06aa9
Poultry Mitra <ipoultrymitra@gmail.com>
2025-10-13 11:03:22 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: ["https://6000-firebase-studio-1760189830759.cluster-ulqnojp5endvgve6krhe7klaws.cloudworkstations.dev"],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
