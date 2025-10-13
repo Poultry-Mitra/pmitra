499023d69509dfe0cac2d635d3d19b3c21847e19
Poultry Mitra <ipoultrymitra@gmail.com>
2025-10-13 11:52:15 +0000

    17:19:40.633 Running build in Washington, D.C., USA (East) – iad1 17:19:

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ["https://6000-firebase-studio-1760189830759.cluster-ulqnojp5endvgve6krhe7klaws.cloudworkstations.dev"],
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
